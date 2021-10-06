af65e86fdd9e60a306edbc862d0c2c7091e7d168
casualshammy <axio@axio.name>
2021-10-06 19:12:06 +0100

    FIX(IBus): Fixed missing methods

--- Ax.Fw/Interfaces/IBus.cs
+++ Ax.Fw/Interfaces/IBus.cs
@@ -11,6 +11,12 @@ namespace Ax.Fw.Interfaces
         void OfReqRes<TReq, TRes>(Func<TReq, Task<TRes>> _func, ILifetime _lifetime)
             where TReq : IBusMsg
             where TRes : IBusMsg;
+        IDisposable OfReqRes<TReq, TRes>(Func<TReq, TRes> _func)
+            where TReq : IBusMsg
+            where TRes : IBusMsg;
+        void OfReqRes<TReq, TRes>(Func<TReq, TRes> _func, ILifetime _lifetime)
+            where TReq : IBusMsg
+            where TRes : IBusMsg;
         IObservable<T> OfType<T>(bool includeLastValue = false) where T : IBusMsg;
         void PostMsg(IBusMsg _data);
         TRes PostReqRes<TReq, TRes>(TReq _req, TimeSpan _timeout)
