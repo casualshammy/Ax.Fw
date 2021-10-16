956e4fc79631a1bbc661247c9e22e43648d1dd47
casualshammy <axio@axio.name>
2021-10-16 13:36:52 +0100

    FIX: Nuget pckg fix

--- Ax.Fw.TcpBus.Tests/EBusTests.cs
+++ Ax.Fw.TcpBus.Tests/EBusTests.cs
@@ -20,44 +20,44 @@ namespace Ax.Fw.Tests
             p_output = output;
         }
 
-        //[Theory]
-        //[InlineData(1)]
-        //[InlineData(10)]
-        //[InlineData(100)]
-        //[InlineData(1000)]
-        //public async Task StressTestClientServer(int _num)
-        //{
-        //    var lifetime = new Lifetime();
-        //    try
-        //    {
-        //        var server = new TcpBusServer(lifetime, new EventLoopScheduler(), 9600, false);
-        //        var client0 = new TcpBusClient(lifetime, new EventLoopScheduler(), 9600);
-        //        var client1 = new TcpBusClient(lifetime, new EventLoopScheduler(), 9600);
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public async Task StressTestClientServer(int _num)
+        {
+            var lifetime = new Lifetime();
+            try
+            {
+                var server = new TcpBusServer(lifetime, new EventLoopScheduler(), 9600, false);
+                var client0 = new TcpBusClient(lifetime, new EventLoopScheduler(), 9600);
+                var client1 = new TcpBusClient(lifetime, new EventLoopScheduler(), 9600);
 
-        //        client0
-        //            .OfReqRes<SimpleMsgReq, SimpleMsgRes>(msg =>
-        //            {
-        //                return new SimpleMsgRes(msg.Code + 1);
-        //            });
+                client0
+                    .OfReqRes<SimpleMsgReq, SimpleMsgRes>(msg =>
+                    {
+                        return new SimpleMsgRes(msg.Code + 1);
+                    });
 
-        //        var counter = 0;
-        //        var sw = Stopwatch.StartNew();
-        //        Parallel.For(0, _num, _ =>
-        //        {
-        //            Interlocked.Increment(ref counter);
-        //            var i = _;
-        //            var result = client1.PostReqResOrDefault<SimpleMsgReq, SimpleMsgRes>(new SimpleMsgReq(i), TimeSpan.FromSeconds(3600));
-        //            Assert.Equal(i + 1, result?.Code);
-        //        });
-        //        p_output.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
-        //        Assert.Equal(_num, counter);
-        //        //Assert.InRange(sw.ElapsedMilliseconds, 0, _num * 150);
-        //    }
-        //    finally
-        //    {
-        //        lifetime.Complete();
-        //    }
-        //}
+                var counter = 0;
+                var sw = Stopwatch.StartNew();
+                Parallel.For(0, _num, _ =>
+                {
+                    Interlocked.Increment(ref counter);
+                    var i = _;
+                    var result = client1.PostReqResOrDefault<SimpleMsgReq, SimpleMsgRes>(new SimpleMsgReq(i), TimeSpan.FromSeconds(3600));
+                    Assert.Equal(i + 1, result?.Code);
+                });
+                p_output.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+                Assert.Equal(_num, counter);
+                //Assert.InRange(sw.ElapsedMilliseconds, 0, _num * 150);
+            }
+            finally
+            {
+                lifetime.Complete();
+            }
+        }
 
 
     }
