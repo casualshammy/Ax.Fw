82a5ee4f3a99ca0eb76ca46da15c2fbcf3b2dbdd
casualshammy <axio@axio.name>
2022-04-15 11:59:05 +0100

    [] (Ax.Fw - ImportClassAggregator): Tried to fix file error

---
+++ Ax.Fw/Attributes/ImportClassAttribute.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System;
+
+namespace Ax.Fw.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ImportClassAttribute : Attribute
+    {
+        public ImportClassAttribute(Type InterfaceType, bool Singleton = false, bool ActivateOnStart = false, bool DisposeRequired = false)
+        {
+            this.InterfaceType = InterfaceType;
+            this.Singleton = Singleton;
+            this.ActivateOnStart = ActivateOnStart;
+            this.DisposeRequired = DisposeRequired;
+        }
+
+        public Type InterfaceType { get; }
+        public bool Singleton { get; }
+        public bool ActivateOnStart { get; }
+        public bool DisposeRequired { get; }
+
+    }
+
+}
--- Ax.Fw/ImportClassAggregator.cs
+++ Ax.Fw/ImportClassAggregator.cs
@@ -12,29 +12,21 @@ using System.Reflection;
 
 namespace Ax.Fw
 {
-    public class ImportClassCollector
+    public class ImportClassAggregator
     {
         private readonly List<object> p_instances = new();
 
-        public ImportClassCollector(ILifetime _lifetime, ServiceCollection _serviceCollection, IEnumerable<Assembly>? _loadReferencedAssembliesFrom = null)
+        public ImportClassAggregator(ILifetime _lifetime, ServiceCollection _serviceCollection, IEnumerable<Assembly>? _loadReferencedAssembliesFrom = null)
         {
             if (_loadReferencedAssembliesFrom != null)
             {
                 foreach (var assembly in _loadReferencedAssembliesFrom.SelectMany(_x => _x.GetReferencedAssemblies()).DistinctBy(_x => _x.FullName))
                 {
                     Assembly.Load(assembly);
-                    Debug.WriteLine($"{nameof(ImportClassCollector)}: Loaded assembly: {assembly?.FullName}");
+                    Debug.WriteLine($"{nameof(ImportClassAggregator)}: Loaded assembly: {assembly?.FullName}");
                 }
             }
 
-            foreach (var type in Utilities.GetTypesWith<AutoActivatorExportAttribute>(true))
-            {
-                var exportInfo = (AutoActivatorExportAttribute)Attribute.GetCustomAttribute(type, typeof(AutoActivatorExportAttribute));
-                if (exportInfo.Singletone)
-                    _serviceCollection = (ServiceCollection)_serviceCollection.AddSingleton(exportInfo.InterfaceType, type);
-                else
-                    _serviceCollection = (ServiceCollection)_serviceCollection.AddTransient(exportInfo.InterfaceType, type);
-            }
             foreach (var type in Utilities.GetTypesWith<ImportClassAttribute>(true))
             {
                 var exportInfo = (ImportClassAttribute)Attribute.GetCustomAttribute(type, typeof(ImportClassAttribute));
@@ -43,18 +35,9 @@ namespace Ax.Fw
                 else
                     _serviceCollection = (ServiceCollection)_serviceCollection.AddTransient(exportInfo.InterfaceType, type);
             }
+
             ServiceProvider = _serviceCollection.BuildServiceProvider();
-            foreach (var type in Utilities.GetTypesWith<AutoActivatorExportAttribute>(true))
-            {
-                var exportInfo = (AutoActivatorExportAttribute)Attribute.GetCustomAttribute(type, typeof(AutoActivatorExportAttribute));
-                if (exportInfo.ActivateOnStart && exportInfo.Singletone)
-                {
-                    var instance = ServiceProvider.GetRequiredService(exportInfo.InterfaceType);
-                    p_instances.Add(instance);
-                    if (exportInfo.DisposeRequired)
-                        _lifetime.DisposeOnCompleted(instance as IDisposable);
-                }
-            }
+
             foreach (var type in Utilities.GetTypesWith<ImportClassAttribute>(true))
             {
                 var exportInfo = (ImportClassAttribute)Attribute.GetCustomAttribute(type, typeof(ImportClassAttribute));
@@ -71,5 +54,4 @@ namespace Ax.Fw
         public IServiceProvider ServiceProvider { get; }
 
     }
-
 }
