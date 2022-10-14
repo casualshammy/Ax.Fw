fab470ff200b06838014e8f54ceb4e57cea638c6
casualshammy <axio@axio.name>
2022-10-14 19:11:55 +0100

    feature (Ax.Fw): added extension method `UseGraceContainer` for `IHostBuilder`

---
+++ Ax.Fw/Extensions/Parts/GraceSrvProviderFactory.cs
@@ -0,0 +1,28 @@
+﻿using Grace.DependencyInjection;
+using Grace.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Ax.Fw.Extensions.Parts;
+
+internal class GraceSrvProviderFactory : IServiceProviderFactory<IInjectionScope>
+{
+    private readonly IInjectionScope p_child;
+
+    public GraceSrvProviderFactory(IInjectionScope _child)
+    {
+        p_child = _child;
+    }
+
+    public IInjectionScope CreateBuilder(IServiceCollection _services)
+    {
+        IInjectionScope injectionScope = p_child.CreateChildScope();
+        injectionScope.Populate(_services);
+        return injectionScope;
+    }
+
+    public IServiceProvider CreateServiceProvider(IInjectionScope _containerBuilder)
+    {
+        return _containerBuilder.Locate<IServiceProvider>();
+    }
+}
