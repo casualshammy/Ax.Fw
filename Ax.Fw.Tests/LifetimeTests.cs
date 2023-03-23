18fbfcd6a08d0286be570bab22c01620420241d4
casualshammy <axio@axio.name>
2023-03-23 10:09:07 +0000

    test(ax.fw): подправил тайминги теста `LifetimeTests.ParallelCompleteAsyncTest`

--- Ax.Fw.Tests/LifetimeTests.cs
+++ Ax.Fw.Tests/LifetimeTests.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+using Ax.Fw.Tests.Tools;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -6,126 +7,125 @@ using Xunit.Abstractions;
 
 namespace Ax.Fw.Tests
 {
-    public class LifetimeTests
+  public class LifetimeTests
+  {
+    private readonly ITestOutputHelper p_output;
+
+    public LifetimeTests(ITestOutputHelper output)
     {
-        private readonly ITestOutputHelper p_output;
+      p_output = output;
+    }
 
-        public LifetimeTests(ITestOutputHelper output)
-        {
-            p_output = output;
-        }
+    [Fact(Timeout = 30000)]
+    public async Task FlowTest()
+    {
+      var lifetime = new Lifetime();
 
-        [Fact(Timeout = 30000)]
-        public async Task FlowTest()
-        {
-            var lifetime = new Lifetime();
+      var counter = 0;
+      lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
+      lifetime.DoOnCompleted(async () =>
+      {
+        Interlocked.Increment(ref counter);
+        await Task.Delay(100);
+      });
 
-            var counter = 0;
-            lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
-            lifetime.DoOnCompleted(async () =>
-            {
-                Interlocked.Increment(ref counter);
-                await Task.Delay(100);
-            });
+      await lifetime.CompleteAsync();
 
-            await lifetime.CompleteAsync();
-
-            Assert.Equal(2, counter);
-
-        }
-
-        [Fact(Timeout = 30000)]
-        public void SyncCompleteTest()
-        {
-            var lifetime = new Lifetime();
-
-            var counter = 0;
-            lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
-            lifetime.DoOnCompleted(async () =>
-            {
-                Interlocked.Increment(ref counter);
-                await Task.Delay(500);
-            });
-
-            lifetime.Complete();
-
-            Assert.Equal(2, counter);
-
-        }
-
-        [Fact(Timeout = 30000)]
-        public void MultipleCompleteTest()
-        {
-            var lifetime = new Lifetime();
-
-            var counter = 0;
-            lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
-            lifetime.DoOnCompleted(async () =>
-            {
-                Interlocked.Increment(ref counter);
-                await Task.Delay(500);
-            });
-
-            lifetime.Complete();
-            lifetime.Complete();
-
-            Assert.Equal(2, counter);
-
-            Thread.Sleep(500);
-            lifetime.Complete();
-            lifetime.Complete();
-        }
-
-        [Fact(Timeout = 30000)]
-        public void ParallelCompleteTest()
-        {
-            var lifetime = new Lifetime();
-
-            var counter = 0;
-            lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
-            lifetime.DoOnCompleted(async () =>
-            {
-                Interlocked.Increment(ref counter);
-                await Task.Delay(500);
-            });
-
-            Parallel.For(0, 100, _ =>
-            {
-                lifetime.Complete();
-            });
-            Thread.Sleep(500);
-            Parallel.For(0, 100, _ =>
-            {
-                lifetime.Complete();
-            });
-
-            Assert.Equal(2, counter);
-        }
-
-        [Fact(Timeout = 30000)]
-        public void ParallelCompleteAsyncTest()
-        {
-            var lifetime = new Lifetime();
-
-            var counter = 0;
-            lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
-            lifetime.DoOnCompleted(async () =>
-            {
-                Interlocked.Increment(ref counter);
-                await Task.Delay(500);
-            });
-
-            Parallel.For(0, 100, _ => lifetime.CompleteAsync());
-
-            Thread.Sleep(250);
-            Assert.Equal(1, counter);
-
-            Thread.Sleep(750);
-            Assert.Equal(2, counter);
-
-            Parallel.For(0, 100, _ => lifetime.CompleteAsync());
-        }
-
+      Assert.Equal(2, counter);
 
     }
+
+    [Fact(Timeout = 30000)]
+    public void SyncCompleteTest()
+    {
+      var lifetime = new Lifetime();
+
+      var counter = 0;
+      lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
+      lifetime.DoOnCompleted(async () =>
+      {
+        Interlocked.Increment(ref counter);
+        await Task.Delay(500);
+      });
+
+      lifetime.Complete();
+
+      Assert.Equal(2, counter);
+
+    }
+
+    [Fact(Timeout = 30000)]
+    public void MultipleCompleteTest()
+    {
+      var lifetime = new Lifetime();
+
+      var counter = 0;
+      lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
+      lifetime.DoOnCompleted(async () =>
+      {
+        Interlocked.Increment(ref counter);
+        await Task.Delay(500);
+      });
+
+      lifetime.Complete();
+      lifetime.Complete();
+
+      Assert.Equal(2, counter);
+
+      Thread.Sleep(500);
+      lifetime.Complete();
+      lifetime.Complete();
+    }
+
+    [Fact(Timeout = 30000)]
+    public void ParallelCompleteTest()
+    {
+      var lifetime = new Lifetime();
+
+      var counter = 0;
+      lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
+      lifetime.DoOnCompleted(async () =>
+      {
+        Interlocked.Increment(ref counter);
+        await Task.Delay(500);
+      });
+
+      Parallel.For(0, 100, _ =>
+      {
+        lifetime.Complete();
+      });
+      Thread.Sleep(500);
+      Parallel.For(0, 100, _ =>
+      {
+        lifetime.Complete();
+      });
+
+      Assert.Equal(2, counter);
+    }
+
+    [Theory(Timeout = 30000)]
+    [Repeat(10)]
+    public async Task ParallelCompleteAsyncTest(int _iteration)
+    {
+      var lifetime = new Lifetime();
+
+      var counter = 0;
+      lifetime.DoOnCompleted(() => Interlocked.Increment(ref counter));
+      lifetime.DoOnCompleted(async () =>
+      {
+        Interlocked.Increment(ref counter);
+        await Task.Delay(500);
+      });
+
+      Parallel.For(0, 100, _ => lifetime.CompleteAsync());
+
+      await Task.Delay(250);
+      Assert.Equal(1, counter);
+
+      await Task.Delay(1000);
+      Assert.Equal(2, counter);
+    }
+
+
+  }
 }
