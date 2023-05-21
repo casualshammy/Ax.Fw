88ec067db49dd8c2a606995b6102f8ba38562054
casualshammy <axio@axio.name>
2023-05-21 20:47:07 +0100

    test(ax.fw): `ObservableExtensions` test fix

--- Ax.Fw.Tests/ObservableExtensionsTests.cs
+++ Ax.Fw.Tests/ObservableExtensionsTests.cs
@@ -1,8 +1,8 @@
-﻿#nullable enable
-using Ax.Fw.Extensions;
+﻿using Ax.Fw.Extensions;
 using System;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -11,28 +11,27 @@ namespace Ax.Fw.Tests;
 
 public class ObservableExtensionsTests
 {
-    private readonly ITestOutputHelper p_output;
+  private readonly ITestOutputHelper p_output;
 
-    public ObservableExtensionsTests(ITestOutputHelper _output)
-    {
-        p_output = _output;
-    }
+  public ObservableExtensionsTests(ITestOutputHelper _output)
+  {
+    p_output = _output;
+  }
 
-    [Fact(Timeout = 3000)]
-    public async Task ScanAsyncTest()
-    {
-        var result = await Observable
-            .Return(Unit.Default)
-            .Repeat(10)
-            .ScanAsync(0, async (_seed, _entry) =>
-            {
-                await Task.Delay(TimeSpan.FromMilliseconds(50));
-                _seed += 1;
-                return _seed;
-            });
+  [Fact(Timeout = 10000)]
+  public async Task ScanAsyncTest()
+  {
+    var result = await Observable
+      .Return(Unit.Default)
+      .Repeat(10)
+      .ScanAsync(0, async (_seed, _entry) =>
+      {
+        await Task.Delay(TimeSpan.FromMilliseconds(50));
+        return Interlocked.Increment(ref _seed);
+      });
 
-        Assert.Equal(10, result);
-    }
+    Assert.Equal(10, result);
+  }
 
 
 }
