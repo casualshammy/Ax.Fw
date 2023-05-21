ac8149bd72c81b066ff7b5a61ba4ed9b6986cadd
casualshammy <axio@axio.name>
2023-05-21 18:58:15 +0100

    test(ax.fw): `FileCache` test fix

--- Ax.Fw.Tests/Cache/FileCacheTests.cs
+++ Ax.Fw.Tests/Cache/FileCacheTests.cs
@@ -93,7 +93,7 @@ public class FileCacheTests
       ms.Position = 0;
 
       await cache.StoreAsync(key, ms, lifetime.Token);
-      await Task.Delay(100);
+      await Task.Delay(250);
 
       var sizeBeforeCleanUp = await new DirectoryInfo(tempDir).CalcDirectorySizeAsync(lifetime.Token);
       Assert.NotEqual(0, sizeBeforeCleanUp);
