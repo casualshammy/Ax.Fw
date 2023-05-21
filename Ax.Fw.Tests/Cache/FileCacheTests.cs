6ab3d9a20cee778fd97e64f14e417b50c2b163d1
casualshammy <axio@axio.name>
2023-05-21 19:50:27 +0100

    test(ax.fw): `FileCache` test fix

--- Ax.Fw.Tests/Cache/FileCacheTests.cs
+++ Ax.Fw.Tests/Cache/FileCacheTests.cs
@@ -74,39 +74,4 @@ public class FileCacheTests
     }
   }
 
-  [Fact(Timeout = 30000)]
-  public async Task AutoCleanUpTextAsync()
-  {
-    await p_semaphore.WaitAsync();
-    try
-    {
-      using var lifetime = new Lifetime();
-      var tempDir = Path.Combine(Path.GetTempPath(), Random.Shared.Next().ToString());
-      var cache = new FileCache(lifetime, tempDir, TimeSpan.FromMilliseconds(500), 1024 * 1024, TimeSpan.FromMilliseconds(100));
-
-      var key = "test-key";
-      var data = new byte[1024];
-      Random.Shared.NextBytes(data);
-
-      using var ms = new MemoryStream();
-      await ms.WriteAsync(data, lifetime.Token);
-      ms.Position = 0;
-
-      await cache.StoreAsync(key, ms, lifetime.Token);
-      await Task.Delay(250);
-
-      var sizeBeforeCleanUp = await new DirectoryInfo(tempDir).CalcDirectorySizeAsync(lifetime.Token);
-      Assert.NotEqual(0, sizeBeforeCleanUp);
-
-      await Task.Delay(TimeSpan.FromMilliseconds(1000));
-
-      var sizeAfterCleanUp = await new DirectoryInfo(tempDir).CalcDirectorySizeAsync(lifetime.Token);
-      Assert.Equal(0, sizeAfterCleanUp);
-    }
-    finally
-    {
-      p_semaphore.Release();
-    }
-  }
-
 }
