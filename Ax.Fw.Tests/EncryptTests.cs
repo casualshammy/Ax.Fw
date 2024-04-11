3ebdf9d1e0435e31c30e1c213db0df606f30ac41
casualshammy <axio@axio.name>
2024-04-11 17:40:10 +0100

    fix(ax.fw - file cache): removed sync `TryStore` feature(ax.fw - crypto): added encrypted private key support for rsa+aes

--- Ax.Fw.Tests/EncryptTests.cs
+++ Ax.Fw.Tests/EncryptTests.cs
@@ -446,12 +446,53 @@ public class EncryptTests
     Random.Shared.NextBytes(data);
 
     var rsaAes = new RsaAesGcm(
-      GetRsaPrivateKey().ToCharArray(), 
-      GetRsaPublicKey().ToCharArray(), 
+      GetRsaPublicKey(),
+      GetRsaPrivateKey(), 
+      null,
       EncryptionKeyLength.Bits256);
 
     var sw = Stopwatch.StartNew();
     var elapsed = 0L;
+
+    sw.Restart();
+    var encryptedData = rsaAes.Encrypt(data);
+    elapsed = sw.ElapsedMilliseconds;
+    p_output.WriteLine($"RsaAes Encrypt: {elapsed}ms");
+    Console.WriteLine($"RsaAes Encrypt: {elapsed}ms");
+
+    var encryptedDataBytes = encryptedData.ToArray();
+    Assert.NotEmpty(encryptedData.ToArray());
+    Assert.NotEqual(data, encryptedDataBytes);
+
+    sw.Restart();
+    var decryptedData = rsaAes.Decrypt(encryptedData);
+    elapsed = sw.ElapsedMilliseconds;
+    p_output.WriteLine($"RsaAes Decrypt: {elapsed}ms");
+    Console.WriteLine($"RsaAes Decrypt: {elapsed}ms");
+
+    var decryptedDataBytes = decryptedData.ToArray();
+    Assert.NotEmpty(decryptedDataBytes);
+    Assert.Equal(data, decryptedDataBytes);
+  }
+
+  [Theory(Timeout = 30000)]
+  [InlineData(512)]
+  [InlineData(1024 * 1024)]
+  [InlineData(1165217)]
+  [InlineData(11652170)]
+  public void RsaAesEncryptedKeyTest(int _taskSize)
+  {
+    var data = new byte[_taskSize];
+    Random.Shared.NextBytes(data);
+
+    var rsaAes = new RsaAesGcm(
+      GetRsaEncryptedPublicKey(),
+      GetRsaEncryptedPrivateKey(),
+      "qwerty125521",
+      EncryptionKeyLength.Bits256);
+
+    var sw = Stopwatch.StartNew();
+    long elapsed;
 
     sw.Restart();
     var encryptedData = rsaAes.Encrypt(data);
@@ -482,6 +523,16 @@ public class EncryptTests
   private static string GetRsaPrivateKey()
   {
     return "-----BEGIN RSA PRIVATE KEY-----\r\nMIIJKwIBAAKCAgEA8jMlwzPn0a1YsaWryiq8Oh2Wn6EkF9wPIQPV7Vd+yR754+vF\r\n8L3yQAE+slKcRGXJtWuLrLN4MIsR0YKNRes3e2XnIGuZNGJ5+JD1IXu27k/H/Q9F\r\nGJKTO1SQ+2RAIPJzV5UuswW9oDIXXD2Sdi1psM0d3Pn46/w+wknH/jIujB9+8MQC\r\nx+BPcpOAZcJtdfxab8reZ2JqwSEmq9U6DakbYTfH/M62Zmw85/Gz5LC1LuMeB6SU\r\nSRM8wUy54Ps7wJ4uHYtlz0hbV1JFGbLgEtBF03RcTOw2qYvdxgPfz3I7T0HlWO6k\r\nObN4Yjc9Oud6LJF1mTeJeaawb27iIWjA0e5BtitBXiubiICJja8FF0oygrRFH4/U\r\n8T7IkATdeFRVrJ1dRhnG6mqbsFGb9dgjXiztONq3teryUR8Td9FLbVD1Vy5pCkIX\r\n1Gox+YWzzCePsZT6o9ZlLoZNS1qpgNVoNP56kIvut34lDx77esyZRba5y+Zma2tc\r\nWB5xDzqWsJ48u9wZAtVrBmJe0LPR2K9AzvMCVIBHFzei6Ayu/gOOuO+3nTf13dnv\r\nNm1SS+CTubMmxBGEKDGo1TnoNi7Tj/v9I5ZyJXfZrlt1/0IJu3oqFvUiY09nTiLa\r\nImFw9R/2Ke0V/0gfxJXlfOGl294NdaLsPYFjUH8LaiVJ9lgl59kSrbQgQYUCAwEA\r\nAQKCAgEAvQMvgDQcwOSgKBsbgu1w8YWvy6nM6hXhdKlypQO4PSrAZ5/TXLpPuKWA\r\nEVgo/bPWA5AHc+KndHLDmBZjO+KB7Possn9mE5yahWJS+yt6Kmb2ssXc7X1OC2pG\r\nrvmgllW/r+ULich8IO2Wj3S5vSJZrhGVMaOfIEM9kxBTVExDSTU3Mpw1c1jZh5gX\r\nBtMB66bhyQawJEyI9WlyrXz9DjYf2PHYT7HeZPYpXfWhp2JEM3ApOlu1IYYyzsOa\r\n+Dn4eqy3XnUwIeDc77uTk6el+Oiy5X+UnK9nRU+S5nqVimYiZQsO+iVa4nDuDPAB\r\nB9wn22o1NTpDPj3YyU3miZ4fhHWErypN3h6xJpWl0fOrJBMRZfzQ1Cka8Vx1gS4T\r\n4GENlqxS2oyWY2LnwRzFT+jV89sUQ5+oXgaF8vcN2Lk6iCWMY/aH460rmUojuieR\r\n1LH+JNDF7W6OC13kFigu+5CoWZBcskkpjul5B4OHso2fxynWnk9dligyjT17RRpg\r\ngfy9v0PEkHnCIUc5X9L20jQuSr/F9WvjGq+ujbhGlcX7uKPv+lY+X0/XQUxJ1o6R\r\nPnCSkLUiLsx5q0GIF/BQzz0N60xN7/26YeaPBdDJgZhAhRKu7Qha+W4QtTxaqcOp\r\nt3Fky6nLzEbybrSBNbOAxPX556YtfeN8fBeYG35t2IzwZKNLpcECggEBAPtAHgqH\r\nTzHFk0KV15aFYgjdBg3U+zuiLdLwQSnjfyfoDFQGKXAxPlXV3qjtZ92SBrfeqLQB\r\n5+4RIMEnFPfv4L6+iLo73nr9VYPfepmACrV+yelkzBC2Nkh7A5LEJAJSDC+ebQs5\r\nooklXw/GfDi2yplNHOWT/WaWjIhCCJGQPMD8UFhzTRFXcePWy2uxaeL7K7SdNOIh\r\nmeg/cJbBE3U5pdn9tgA+LFe36VoprMG95dn6D9TIR+wT40m5gTXjEvkFncHq758t\r\nIolsR9/AfJAZmfp2v7yX/zTHKc2SevXSESL4Xf3UkByxVutv6WMKTWgVYbx9Xpo2\r\neFsu5AQLLSCiezECggEBAPbHOyasdLzOhYP6uZY1DvB/LK8Jp3DeU/Y6KnZnG2el\r\nO54D/HXq8r06v1q1TOxBEbtREnc+SYCcT8UUfwdlXjxPxcDxtO0F8GNBDI6DLpG0\r\n3xqbrUPYLpeS/lc9CiEKpY6VWslK7owPTzOGNLROYBP7pvqLZY39tJZDMiRAHrwc\r\nZAMiPpAZKMM+Z0Ak3FXdTGUCveA7oN32qY6xNnX/x4O583kYT0QXoptblcGCFrbS\r\nW66MLDQmaiXaRDOwUc3I40ps3MXmfBF6mOOyFt9TYMSf5pLvhvOK2vtmBoOuFDkb\r\nqGQWM8k0hF0P+nv2eMkHCUIwqrlPT5sLAvUQYke37pUCggEBAOqoeqZIs91/c0cY\r\nHc92ahZvH81hYvfSQ8wkGiheUo9Z+dGsI52mUFrosdnCSWS2ktG+AoCE2zINCzN7\r\nJelfP9/GonqVmffyjaDp415kKRxT+46hVroxBEfzpGW6DQuhx8HdzGhUfwpqPfvE\r\n3JY3msdNLzT4YA4lg8FrMweI2EVCImRJ4+vTaQprvXeqroORJMO+o/qjeVRhk+0p\r\nDNxKaC1N+WMGrnGK0kYkcDZO/tLz6z9Hg2zpMjnUKOrEVx7/cNUBcKWRCRWibQp8\r\ng5ouXxJ/QBqLN476iH94VKEsflbT8y00DxjNRKoFzstyftM8TRk3WljbkNNQ2yMv\r\nsQku17ECggEBAJznXdegDPVDBic9EiTMBXyqD3oXVEvyQEYtA3SW6BjFORumAy3i\r\nPYZNRP9iiM8qLCECUhBZHRjVye5Psti7/eew/NZJmDSf8xDYeihehgyEiNn9I3Qg\r\njrc28dn76GXLxCndEoUrzHJnBX+IFBiUQIhUF/3zBZX7OofTn5zm2+PU1U4cxtSN\r\n+pnxImrpROKfOwR6csmQgB7Ax1v/ltX91BgP8hsLdtKfiHbpC1Wm5dRF+Z8IZs6d\r\noh4BIh4PPGPwF3vprZFyk231miIjyGEkPUGnPU4tV4ufvM3dN8UfuKH60N0aNMD4\r\nUDRvhV6t/mwquhtIoDQElmPI4493ZFWfP+UCggEBAK6OBFGVBxTardwI5VURwJLr\r\nvB0MA4Fmuwqg09Ny9FrjklJ914VCwakLALkTzCNiIrp5wPW21nCg1pJ+2Ayx4zW1\r\nJsJ4zUYKXshNRbK/nQsMMFmo66jW6TVCbIowogsdCVwQFO8VWMLUi6danZ3ObsBs\r\nFWg6CBUcdcvpjPk1KozckZtv75cUOtfY+wi+7oOrTht+nhfgIE8TPn5DvecrS6iX\r\nz+sCPu1kNv/y1FyrDiiKx2h+mn6zlh3S6OHAFwRtj9xHSLH8aVTsdqz3oT3lEu3t\r\n1Fv7UgdoUfP3PMopAltyJxmAWd+yr9cVjbkXhYUimbIdGxpeEdPqtOAwLVXyY1o=\r\n-----END RSA PRIVATE KEY-----\r\n";
+  }
+
+  private static string GetRsaEncryptedPublicKey()
+  {
+    return "-----BEGIN PUBLIC KEY-----\r\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAt3gGpuZQyr2LCfDeEQa3\r\n72i6cIh8U3iovqB5LGOkkh6KepuzK/ENFHOoRBQHPYSGbyeqn601j+L5xOm5Bwlr\r\nz5yo+TCj1e1vX4eJrJ+TFBq8K6Hl49ROdldDak7oJA/3vOWExvWy4l+rjbUiKRvS\r\nDVH+CZZxq5KsNEJ6EoVvers3u6hXPC0gHqHuAgPsiSTe088njnmY8AlVbpBxSNEA\r\nixPFqn//ekr1Lx1pTZnL22J2nWSU6bHERQQndFlVyUTbX0F3a8WucW5n3X0LA6mI\r\nE4HRtvEQfli+KluLyx7SiIYuiE3I7w/vZ4OqXIa1gcnA2HH2jsK+KKIUrj13AK1M\r\nIvuY6xYfcPWG/7LvoAzf1NXupXvmR83NBUuTyBoa8aanKWOSs/ct5+pCVt2Nee6m\r\nlGfNAz02gyAubaIx0cWgdo7Cv0ahz0cBgNW5qQFCpJ1nG6YkgA9KWAeBcifKNXOU\r\n5QvBFgstf9mRZBmcjP0NIFXbdkKRu/a9bjrwgH15jvF8+K/FWyQCqwREKy5Alotz\r\nnzCk8eHGUO8R1EIeI1Yx5OPMvogJG29EqkdT+2TrBxqxjmrDVAelTZwwg4qWVS1w\r\n+tzCfG+pI5ARLG154uS64AZBa9ytAjjxXfLorrN7NBM3gj4AhQiBDOqA88/B43yi\r\nKz8SGGw+sKlFvYH7ufcICqMCAwEAAQ==\r\n-----END PUBLIC KEY-----\r\n";
+  }
+
+  private static string GetRsaEncryptedPrivateKey()
+  {
+    return "-----BEGIN ENCRYPTED PRIVATE KEY-----\r\nMIIJaTAbBgkqhkiG9w0BBQMwDgQICt3v025AbLwCAggABIIJSP20iSw8iFzU3ROJ\r\ngPoR91nOpi5Iw/7LX+/Mq08erpx0eBx9cTRwfj2JNn0W5PwCele7Nzzux8MMuJZK\r\nLN9A0tcB2+O80hlKGz/uzim4fdrtqV6x6VASaNY8ycZmlRq+d1qjJJzKE5YOUUSh\r\nTu7/5TElL9UMEt3bBlFytIDh9cDryJVTJ4qc91VP9TbBMksCrMzKmR4Cco2iGr0T\r\nzqPjQgp9t1GTUTlpkcqvypXRlynylOoEUEoRDUtsZ9HOrpVjD0gs5+0KsTo/uZgM\r\n0Vr2SKqnX6chFs+aYFAThYKvOXfdptdqK6dGSrDqV0v0MrZcL/86aUboSuHtiH8i\r\n0Ste9i06PJA9JROU4qYwSiL44Iwvx1Us7W2z9E52U0qfGjZeyvCvQuzMO+3A9IF/\r\nngj7SzSulq5EJoDdHqQTW5soaPanYqB8w5fA2ZWzWjKjf/7JQNJgdfdMRg9y4buA\r\nh8ebfbTK3M53fC7sfdHD3SWwHEyUooe/T2wKAeNjxTXRq2uDjJuvWSlytrr2Qvez\r\nMZ1QInGFFQveiyHEwcm9TAypja7l24Jm0bkyeaoeb1jcra3sPntmUAFhw49UVv8J\r\ntEElBZUKRD9LAqPmSH7Jna9I4B5ZQ17NaWGQ30vDleqJMzs3THYekD4JOZs1c7um\r\nuNZii7rzxp61u3LyF+JQNYGAKMjsh0QI7//oPOh73DNBu/h/voOGAk12Z+AjjlUi\r\n4Zr5T8RDoMwSTmOdgkbRCgRebacbW1fgUDCz0ZjBv5a8ebT/PRzs5CJ/EQoFIi8t\r\nJBuma0mWr+4O1mgQUP/YKHRqvZm9A1nkH1Qfpcmz6pPZ9a6n+BtWyhSrvnjuIa3D\r\n8r0bKbCTL54hIrVmOZL2+xcHAaUuasR20LNDZVzByRz3CrQUE4M0bmz8pfxCeBMM\r\nf+k5VyEC1uPgfUyRrvj8crh7F1VCF0q2GtZJvJaRTJbvFtAyFiweRS2zy/CPfsKe\r\nz324tNkrYuzvqQEJnpNqFuIigCW6QjJzcXljJ1jFD41bBFyxrVOrNk6NMn4lxMVd\r\nYvhojBluAxz49gonMPtsxzWwUr8V0/DGimV66jL5rmuN7zJo7d2bdpHr4+Fi/R3E\r\nP9h40wk3aKBwsUrA6/HhVs5E/6iedqruR4wX+Xs6TLLfAM86QtsnxwmmT9UKdify\r\n0WukxDp1ASf+lOauZdyN2lgRey1haFIutMBpJUKaomZ3xLQA38+VzKm+/at7vvfd\r\nyTyKe4fvDEaBCXrMUoHjdl3CUbQ2zkoHArj8pZSaZiEcu5uOkJ3zG0AnoEwrOAe5\r\npgJnEXd9/38cjLjg6XNh04rXrJm2MVNkYwkgGKC5yk4e8xDVxR0j083ovoC2Whaa\r\nVt1WYwe4LxwbyBudRoq1GqiWLFKXbp2CcKng8ZccKzoLBgnxCi2coNYL4sAyJRe6\r\nd60FhVXejZZnSzdm1k6DF+lMo71DQ/wmV9xRK2oAWlIDwmjKu1QBln7dlHw0qggR\r\nK9Q8nW80usaFihHJ/vyui02tEh3dEaXhi1bYpSjCWIGZfFH7oXaDQ6DMwaJ9T3t7\r\nVDc9u8latgP+5rEi8edZxq5sA2Owno1xyfjQnqbJ8T056GGU5b5xH/toEaV1GQhi\r\nYmsA0FBPaqs7YZHjTqBb1W9NaocHsW3Bbp/mhz9LWuOhSctRmLKFFA+5RzP2DevE\r\nCxEDFHhXWXafb7IK3/iV7QI2kwCPq3Amp5r3ZzDtu0cv/WpcPYf6WOOfnVLySAc+\r\nM3O+A4dhIY88m6a/cgJX7chC/r+B28O4CAeWMDm+kWU3zmXZ5TWGbyWCyHeJuHKg\r\nXX+gfUfX8o6o4EBNopW+neixvMcLZ+LWp2sJ/NrjM+6TujQA24xKYivB/H/NSPiT\r\naC9G42MpPdUY9wHuyWrmOv2qVs+GlW/kVM4pK8SWEdg1L7Y6oP3oLjWh0xj/3EZr\r\nC2ydodwAfleHfcw671wEpa4q6pCap9NfqHVpX1DFbSxEefKQBOba3oZupAEDuwok\r\nlBREW6W3mGa0Ni4cAgW7+x9v0dtYtvav8MxCf9RugvCiOYwqLp2gZ3N/2uXfDMAN\r\n4Cq3CjVX/2ECjNXSeGlLPGW75gzWlwgYJKexI/3foY+xmgMy+GGR1pmXnwk9ixTy\r\nqa9VZ+ROqSNV2hjOdYsfTmfB/T3QlN37IW+U/yUuuaY+aEfSAZDfyc4FGNrqtXQJ\r\nesXx0SEyNEiRdzq6U4JEnykBzB98jUcuSxtUvJRkZ/prGaCAh13aUksqH5foS5mC\r\neruNefs8IlaltcN7SW2WQAps3L2zU/daqzDzppGCPeObTgWsq00yo0cG5JbK3DIN\r\nkgogJslpEmHkAYt4CrbfR49MjIGUN5sY5edED0it3cQI2K2Vpp+OreNGSrvtFvO4\r\n9q5PKjARyQa1lWjdGVKSz92Y2IMcUgKnuqGXu/1aQ+NIu8uwWGUJNIuxq6YVtmdv\r\n6+NHP8m9OX3D4GZGeZWSVqKxSV1klrTJx94J/frIUwbV39o8IeQTzpKhSGOgpkR1\r\n0Xj/rjJd/rV91MKkBCFQ9wGN78YZ5jCQqgm7PKecJomHzDBdRT8yv+/3Lc5y4J8B\r\n2tMJOxkhqzv8I+cZnQlrk+V8gFu/Z1JPvpmWuvx4efkA5PCalA6yk8fbUQ7t3mEh\r\nZ3Ck1cTF66NduFIUcBB8pcfXcvNueorvn+k/TfyTrAKG4aPOCC05EtYLMAleAgYM\r\nI7b+sjMxPkc11R0HGmlezqQk00BMKbBDht20IDVfNGNwxafSdHJ9V2QLAngKhN6G\r\nzZO5LgR5HOoR0Zub3E3DqevFMTOqiu4kmqEqyTSgWvUHumz1ngivaa0zUEj0ft/W\r\nP0zDE93kbk37VSdR54lenLckrF1AkGONhWYidfuboXc6bMKROcwdTFUf1u4Ru8vm\r\nZIHWfWnv2HMMo4avMl/5Ix2wkasIz5abVpnZT1SKgbEam1NLU4ZJ+rsvbvD98+/Z\r\nSqP+FWIkc0eepLfP6e0r3HG6Xe5pMNb/ZHhEt2M1HaCehple1X/fEZmE5PQzoNxi\r\nBa9ilZfd/HnPOvGvPz+EVrrVdHn70Ij1Fh6+qjyKLfSKEq4oJuIDl1sGfwyCSwRQ\r\nb/EPNDvBrKQIZGctdRflf3xkhzCnsQdinFXdHPGYSU+CQd/9jjPuZlbDoSqeNbVh\r\nLD/vygFHqIbrh/aj9w==\r\n-----END ENCRYPTED PRIVATE KEY-----\r\n";
   }
 
 }
--- Ax.Fw/Crypto/RsaAesGcm.cs
+++ Ax.Fw/Crypto/RsaAesGcm.cs
@@ -3,37 +3,58 @@ using Ax.Fw.SharedTypes.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Ax.Fw.Crypto;
 
 public class RsaAesGcm : DisposableStack, ICryptoAlgorithm
 {
   private readonly RSACryptoServiceProvider p_rsa;
-  private readonly char[]? p_privateKeyPem;
-  private readonly char[]? p_publicKeyPem;
+  private readonly string? p_publicKeyPem;
+  private readonly string? p_privateKeyPem;
+  private readonly byte[]? p_privateKeyPassword;
   private readonly EncryptionKeyLength p_keyLength;
 
+  // openssl genrsa -out private_raw.pem 4096
+  // openssl pkcs8 -topk8 -inform PEM -outform PEM -in private_raw.pem -out private.pem -passout pass:<your-password>
+  // openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+
+  /// <summary>
+  /// RSA + AES-GCM encryption
+  /// </summary>
+  /// <param name="_publicKeyPem">PEM-encoded public key (NOT a path to file!)</param>
+  /// <param name="_privateKeyPem">PEM-encoded private key (NOT a path to file!)</param>
+  /// <param name="_privateKeyPassword">Pay attention: only PKCS#8 encrypted private keys are supported</param>
+  /// <param name="_aesKeyLength"></param>
   public RsaAesGcm(
-    char[]? _privateKeyPem,
-    char[]? _publicKeyPem,
+    string? _publicKeyPem,
+    string? _privateKeyPem,
+    string? _privateKeyPassword,
     EncryptionKeyLength _aesKeyLength = EncryptionKeyLength.Bits256)
   {
     p_rsa = ToDisposeOnEnded(new RSACryptoServiceProvider());
-    p_privateKeyPem = _privateKeyPem;
     p_publicKeyPem = _publicKeyPem;
+    p_privateKeyPem = _privateKeyPem;
+    p_privateKeyPassword = _privateKeyPassword != null ? Encoding.UTF8.GetBytes(_privateKeyPassword) : null;
     p_keyLength = _aesKeyLength;
   }
 
   public Span<byte> Encrypt(ReadOnlySpan<byte> _data)
   {
-    if (!(p_publicKeyPem?.Length > 0))
-      throw new InvalidOperationException("Public key is not set");
+    if (p_publicKeyPem == null && p_privateKeyPem == null)
+      throw new InvalidOperationException("You must provide either public or private key to encrypt data");
 
     var symmetricKey = RandomNumberGenerator.GetBytes(32);
     using var aesGcm = new AesWithGcm(symmetricKey, p_keyLength);
     var encData = aesGcm.Encrypt(_data);
 
-    p_rsa.ImportFromPem(p_publicKeyPem);
+    if (p_publicKeyPem != null)
+      p_rsa.ImportFromPem(p_publicKeyPem);
+    else if (p_privateKeyPassword != null)
+      p_rsa.ImportFromEncryptedPem(p_privateKeyPem, p_privateKeyPassword);
+    else
+      p_rsa.ImportFromPem(p_privateKeyPem);
+
     var encSymmetricKey = p_rsa.Encrypt(symmetricKey, RSAEncryptionPadding.Pkcs1);
 
     Span<byte> result = new byte[encSymmetricKey.Length + encData.Length];
@@ -48,7 +69,11 @@ public class RsaAesGcm : DisposableStack, ICryptoAlgorithm
     if (!(p_privateKeyPem?.Length > 0))
       throw new InvalidOperationException("Private key is not set");
 
-    p_rsa.ImportFromPem(p_privateKeyPem);
+    if (p_privateKeyPassword != null)
+      p_rsa.ImportFromEncryptedPem(p_privateKeyPem, p_privateKeyPassword);
+    else
+      p_rsa.ImportFromPem(p_privateKeyPem);
+
     var keySizeBytes = p_rsa.KeySize / 8;
 
     var encSymmetricKey = _data[..keySizeBytes];
