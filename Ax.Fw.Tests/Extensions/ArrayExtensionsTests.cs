8ce7654ad880f4dbbe72d0c48c47b4f9cef01c92
casualshammy <axio@axio.name>
2024-06-06 16:48:55 +0100

    feature(ax.fw): added array deconstruction extensions

---
+++ Ax.Fw.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using Ax.Fw.Extensions;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Ax.Fw.Tests.Extensions;
+
+public class ArrayExtensionsTests
+{
+  private readonly ITestOutputHelper p_output;
+
+  public ArrayExtensionsTests(ITestOutputHelper _output)
+  {
+    p_output = _output;
+  }
+
+  [Fact]
+  public void TestArrayDeconstuction()
+  {
+    var array0 = Array.Empty<int>();
+    var array1 = new int[] { 1 };
+    var array2 = new int[] { 1, 2 };
+    var array3 = new int[] { 1, 2, 3 };
+    var array4 = new int[] { 1, 2, 3, 4 };
+    var array5 = new int[] { 1, 2, 3, 4, 5 };
+
+    Assert.Throws<InvalidOperationException>(() =>
+    {
+      var (a1, a2) = array0;
+    });
+    Assert.Throws<InvalidOperationException>(() =>
+    {
+      var (a1, a2) = array1;
+    });
+    Assert.Throws<InvalidOperationException>(() =>
+    {
+      var (a1, a2, a3, a4, a5) = array4;
+    });
+
+    {
+      var (a1, a2) = array2;
+      Assert.Equal(1, a1);
+      Assert.Equal(2, a2);
+    }
+
+    {
+      var (a1, a2, a3) = array3;
+      Assert.Equal(1, a1);
+      Assert.Equal(2, a2);
+      Assert.Equal(3, a3);
+    }
+
+    {
+      var (a1, a2, a3, a4) = array4;
+      Assert.Equal(1, a1);
+      Assert.Equal(2, a2);
+      Assert.Equal(3, a3);
+      Assert.Equal(4, a4);
+    }
+
+    {
+      var (a1, a2, a3, a4, a5) = array5;
+      Assert.Equal(1, a1);
+      Assert.Equal(2, a2);
+      Assert.Equal(3, a3);
+      Assert.Equal(4, a4);
+      Assert.Equal(5, a5);
+    }
+
+    {
+      var (a1, a2) = array5;
+      Assert.Equal(1, a1);
+      Assert.Equal(2, a2);
+    }
+
+  }
+
+}
---
+++ Ax.Fw/Extensions/ArrayExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Ax.Fw.Extensions;
+
+public static class ArrayExtensions
+{
+  public static void Deconstruct<T>(
+    this T[] _array, 
+    out T _item0, 
+    out T _item1)
+  {
+    if (_array.Length < 2)
+      throw new InvalidOperationException($"Array contains insufficient number of elements: {_array.Length}");
+
+    _item0 = _array[0];
+    _item1 = _array[1];
+  }
+
+  public static void Deconstruct<T>(
+    this T[] _array, 
+    out T _item0, 
+    out T _item1, 
+    out T _item2)
+  {
+    if (_array.Length < 3)
+      throw new InvalidOperationException($"Array contains insufficient number of elements: {_array.Length}");
+
+    _item0 = _array[0];
+    _item1 = _array[1];
+    _item2 = _array[2];
+  }
+
+  public static void Deconstruct<T>(
+    this T[] _array, 
+    out T _item0, 
+    out T _item1, 
+    out T _item2, 
+    out T _item3)
+  {
+    if (_array.Length < 4)
+      throw new InvalidOperationException($"Array contains insufficient number of elements: {_array.Length}");
+
+    _item0 = _array[0];
+    _item1 = _array[1];
+    _item2 = _array[2];
+    _item3 = _array[3];
+  }
+
+  public static void Deconstruct<T>(
+    this T[] _array, 
+    out T _item0, 
+    out T _item1, 
+    out T _item2, 
+    out T _item3, 
+    out T _item4)
+  {
+    if (_array.Length < 5)
+      throw new InvalidOperationException($"Array contains insufficient number of elements: {_array.Length}");
+
+    _item0 = _array[0];
+    _item1 = _array[1];
+    _item2 = _array[2];
+    _item3 = _array[3];
+    _item4 = _array[4];
+  }
+
+}
