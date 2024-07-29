4a90bc23d3444ba3c2a0fa2cd734041acce58b82
casualshammy <axio@axio.name>
2024-07-29 16:50:16 +0100

    fix(ax.fw - file cache): added workaround for cases when user requires stream position=0 and stream length=data length

---
+++ Ax.Fw.Tests/Streams/StreamWrapperTests.cs
@@ -0,0 +1,173 @@
+﻿using Ax.Fw.Streams;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Ax.Fw.Tests.Streams;
+
+public class StreamWrapperTests
+{
+  private readonly ITestOutputHelper p_output;
+
+  public StreamWrapperTests(ITestOutputHelper _output)
+  {
+    p_output = _output;
+  }
+
+  [Fact(Timeout = 10000)]
+  public async Task PositionAndLengthAndDataAsync()
+  {
+    using var lifetime = new Lifetime();
+
+    var data = new byte[1024];
+    Random.Shared.NextBytes(data);
+    using var ms = new MemoryStream();
+    await ms.WriteAsync(data, lifetime.Token);
+    ms.Position = 0;
+
+    {
+      ms.Position = 0;
+      var wrapper = new StreamWrapper(ms, null, false);
+      Assert.Equal(0, wrapper.Position);
+      Assert.Equal(data.Length, wrapper.Length);
+
+      Memory<byte> result = new byte[data.Length];
+      for (var i = 0; i < 4; i++)
+        await wrapper.ReadExactlyAsync(result[(i * 256)..((i + 1) * 256)], lifetime.Token);
+
+      Assert.Equal(data, result);
+    }
+
+    {
+      ms.Position = data.Length / 2;
+      var length = data.Length / 2;
+      var wrapper = new StreamWrapper(ms, null, false);
+      Assert.Equal(0, wrapper.Position);
+      Assert.Equal(length, wrapper.Length);
+
+      Memory<byte> result = new byte[length];
+      for (var i = 0; i < 2; i++)
+        await wrapper.ReadExactlyAsync(result[(i * 256)..((i + 1) * 256)], lifetime.Token);
+
+      Assert.Equal(data[length..], result);
+    }
+
+    {
+      ms.Position = 0;
+      var length = data.Length / 2;
+      var wrapper = new StreamWrapper(ms, length, false);
+      Assert.Equal(0, wrapper.Position);
+      Assert.Equal(length, wrapper.Length);
+
+      Memory<byte> result = new byte[length];
+      for (var i = 0; i < 2; i++)
+        await wrapper.ReadExactlyAsync(result[(i * 256)..((i + 1) * 256)], lifetime.Token);
+      Assert.Equal(data[..length], result);
+    }
+
+    {
+      ms.Position = data.Length / 2;
+      var length = data.Length / 2;
+      var wrapper = new StreamWrapper(ms, length, false);
+      Assert.Equal(0, wrapper.Position);
+      Assert.Equal(length, wrapper.Length);
+
+      Memory<byte> result = new byte[length];
+      for (var i = 0; i < 2; i++)
+        await wrapper.ReadExactlyAsync(result[(i * 256)..((i + 1) * 256)], lifetime.Token);
+      Assert.Equal(data[^length..], result);
+    }
+
+  }
+
+  [Fact(Timeout = 10000)]
+  public async Task UnderlyingStreamDisposeAsync()
+  {
+    using var lifetime = new Lifetime();
+
+    {
+      using var ms = GetStream();
+      var wrapper = new StreamWrapper(ms, null, false);
+      wrapper.Dispose();
+      Assert.Equal(5, wrapper.Seek(5, SeekOrigin.Begin));
+
+      await wrapper.DisposeAsync();
+      Assert.Equal(5, wrapper.Seek(5, SeekOrigin.Begin));
+    }
+
+    {
+      using var ms = GetStream();
+      var wrapper = new StreamWrapper(ms, null, true);
+      wrapper.Dispose();
+      Assert.Throws<ObjectDisposedException>(() => wrapper.Seek(5, SeekOrigin.Begin));
+    }
+
+    {
+      using var ms = GetStream();
+      var wrapper = new StreamWrapper(ms, null, true);
+      await wrapper.DisposeAsync();
+      Assert.Throws<ObjectDisposedException>(() => wrapper.Seek(5, SeekOrigin.Begin));
+    }
+  }
+
+  [Fact(Timeout = 10000)]
+  public async Task SeekAsync()
+  {
+    using var lifetime = new Lifetime();
+
+    var data = new byte[1024];
+    Random.Shared.NextBytes(data);
+    using var ms = new MemoryStream();
+    await ms.WriteAsync(data, lifetime.Token);
+    ms.Position = 0;
+
+    {
+      ms.Position = data.Length / 2;
+      var length = data.Length / 2;
+      var wrapper = new StreamWrapper(ms, length, false);
+
+      wrapper.Seek(32, SeekOrigin.Begin);
+      var result = new byte[length - 32];
+      await wrapper.ReadExactlyAsync(result, lifetime.Token);
+      Assert.Equal(data[(length + 32)..], result);
+    }
+
+    {
+      ms.Position = data.Length / 2;
+      var length = data.Length / 2;
+      var wrapper = new StreamWrapper(ms, length, false);
+
+      wrapper.Seek(32, SeekOrigin.Begin);
+      wrapper.Seek(32, SeekOrigin.Current);
+      var result = new byte[length - 64];
+      await wrapper.ReadExactlyAsync(result, lifetime.Token);
+      Assert.Equal(data[(length + 64)..], result);
+    }
+
+    {
+      ms.Position = data.Length / 2;
+      var length = data.Length / 2;
+      var wrapper = new StreamWrapper(ms, length, false);
+
+      wrapper.Seek(32, SeekOrigin.End);
+      var result = new byte[32];
+      await wrapper.ReadExactlyAsync(result, lifetime.Token);
+      Assert.Equal(data[^32..], result);
+    }
+
+
+  }
+
+  private static MemoryStream GetStream()
+  {
+    var data = new byte[1024];
+    Random.Shared.NextBytes(data);
+    var ms = new MemoryStream();
+    ms.Write(data);
+    ms.Position = 0;
+    return ms;
+  }
+
+}
---
+++ Ax.Fw/Streams/StreamWrapper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ax.Fw.Streams;
+
+public class StreamWrapper : Stream
+{
+  private readonly Stream p_underlyingStream;
+  private readonly bool p_disposeUnderlyingStream;
+  private readonly long? p_length;
+  private readonly long p_initialPosition;
+  private long p_relativePosition = 0;
+
+  public StreamWrapper(
+    Stream _underlyingStream,
+    long? _length,
+    bool _disposeUnderlyingStream)
+  {
+    p_underlyingStream = _underlyingStream;
+    p_disposeUnderlyingStream = _disposeUnderlyingStream;
+    p_length = _length;
+    p_initialPosition = _underlyingStream.Position;
+  }
+
+  public override bool CanRead => p_underlyingStream.CanRead;
+
+  public override bool CanSeek => p_underlyingStream.CanSeek;
+
+  public override bool CanWrite => p_underlyingStream.CanWrite;
+
+  public override long Length => p_length ?? (p_underlyingStream.Length - p_initialPosition);
+
+  public override long Position
+  {
+    get => p_relativePosition;
+    set
+    {
+      if (value < 0)
+        throw new InvalidOperationException($"Position < 0");
+
+      var absolutePosition = value + p_initialPosition;
+
+      var length = Length + p_initialPosition;
+      if (absolutePosition >= length)
+        throw new InvalidOperationException($"Position >= stream length");
+
+      p_relativePosition = value;
+      p_underlyingStream.Position = absolutePosition;
+    }
+  }
+
+  public override void Flush() => p_underlyingStream.Flush();
+
+  public override int Read(byte[] _buffer, int _offset, int _count)
+  {
+    var length = Length;
+    var count = (int)Math.Min(_count, length - p_relativePosition);
+
+    if (count <= 0)
+      return 0;
+
+    var bytesRead = p_underlyingStream.Read(_buffer, _offset, count);
+    p_relativePosition += bytesRead;
+    return bytesRead;
+  }
+
+  public override long Seek(long _offset, SeekOrigin _origin)
+  {
+    var relativePosition = 0L;
+    if (_origin == SeekOrigin.Begin)
+      relativePosition = _offset;
+    else if (_origin == SeekOrigin.Current)
+      relativePosition = p_relativePosition + _offset;
+    else if (_origin == SeekOrigin.End)
+      relativePosition = Length - _offset;
+
+    Position = relativePosition;
+    return relativePosition;
+  }
+
+  public override void SetLength(long _value) => throw new NotImplementedException();
+
+  public override void Write(byte[] _buffer, int _offset, int _count)
+  {
+    var length = Length;
+    var count = (int)Math.Min(_count, length - p_relativePosition);
+
+    if (count <= 0)
+      return;
+
+    p_underlyingStream.Write(_buffer, _offset, count);
+    p_relativePosition += _count;
+  }
+
+  protected override void Dispose(bool _disposing)
+  {
+    if (p_disposeUnderlyingStream)
+      p_underlyingStream.Dispose();
+  }
+
+  public override async ValueTask DisposeAsync()
+  {
+    if (p_disposeUnderlyingStream)
+      await p_underlyingStream.DisposeAsync();
+  }
+
+}
