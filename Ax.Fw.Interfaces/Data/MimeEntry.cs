8850b3e68b2dfb943a59c376576e539ab8e97995
casualshammy <axio@axio.name>
2026-01-16 18:38:03 +0300

    up(mime entry): optimized mime lookup

--- Ax.Fw.Interfaces/Data/MimeEntry.cs
+++ Ax.Fw.Interfaces/Data/MimeEntry.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Ax.Fw.SharedTypes.Data;
 
 public sealed record MimeEntry
 {
-  private static readonly HashSet<MimeEntry> p_allEntries = [];
+  private static readonly ConcurrentDictionary<string, MimeEntry> p_entryPerMime = [];
+  private static readonly ConcurrentDictionary<string, MimeEntry> p_entryPerExt = [];
 
   public MimeEntry(string[] _extensions, string _mime)
   {
     Extensions = _extensions;
     Mime = _mime;
 
-    p_allEntries.Add(this);
+    p_entryPerMime.AddOrUpdate(_mime, this, (_ ,_) => this);
+
+    foreach (var ext in _extensions)
+      p_entryPerExt.AddOrUpdate(ext, this, (_, _) => this);
   }
 
   public string[] Extensions { get; init; }
   public string Mime { get; init; }
 
-  public static IReadOnlySet<MimeEntry> AllEntries { get; } = p_allEntries;
+  public static IReadOnlyDictionary<string, MimeEntry> ByMime => p_entryPerMime;
+  public static IReadOnlyDictionary<string, MimeEntry> ByExtension => p_entryPerExt;
+
 }
