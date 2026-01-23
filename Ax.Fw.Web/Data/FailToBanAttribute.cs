8bed9207dea5572ea60cc7f0c36ce52b5a43f464
casualshammy <axio@axio.name>
2026-01-24 02:13:29 +0300

    up(ax.fw.web - fail to ban middleware): upgraded

--- Ax.Fw.Web/Data/FailToBanAttribute.cs
+++ Ax.Fw.Web/Data/FailToBanAttribute.cs
@@ -5,12 +5,22 @@ namespace Ax.Fw.Web.Data;
 [AttributeUsage(AttributeTargets.Method)]
 public class FailToBanAttribute : Attribute
 {
-  public FailToBanAttribute(int _maxFailedRequests = 10, params HttpStatusCode[] _bannedHttpCodes)
+  public FailToBanAttribute(
+    int _maxFailedRequests = 10,
+    int _banTimeSec = 10 * 60,
+    params HttpStatusCode[] _bannedHttpCodes)
   {
+    if (_maxFailedRequests < 2)
+      throw new ArgumentOutOfRangeException(nameof(_maxFailedRequests), "Must be at least 2");
+    if (_banTimeSec < 1)
+      throw new ArgumentOutOfRangeException(nameof(_banTimeSec), "Must be at least 1 sec");
+
     MaxFailedRequests = _maxFailedRequests;
+    BanTimeSec = _banTimeSec;
     BannedHttpCodes = _bannedHttpCodes.ToHashSet();
   }
 
   public int MaxFailedRequests { get; }
+  public int BanTimeSec { get; }
   public IReadOnlySet<HttpStatusCode> BannedHttpCodes { get; }
 }
